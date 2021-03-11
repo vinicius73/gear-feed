5b706f517fa9ce58d5a039fe8fc1f6da622b25db
Vinicius Reis <luiz.vinicius73@gmail.com>
2021-03-11 15:02:47 -0300

    ✨ hash and Key of news entry

--- src/news/news.go
+++ src/news/news.go
@@ -1,9 +1,28 @@
 package news
 
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
 // Entry news
-type Entry = struct {
+type Entry struct {
 	Title string
 	Link  string
 	Image string
 	Type  string
+}
+
+// Hash of entry
+func (e Entry) Hash() string {
+	h := md5.New()
+
+	h.Write([]byte(e.Link))
+
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// Key of entry
+func (e Entry) Key() string {
+	return e.Type + ":" + e.Title
 }
