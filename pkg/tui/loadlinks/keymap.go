0fd617d1e61295c5c2cfcfb132e0262674f5e35e
Vinicius Reis <luiz.moreira@willbank.com.br>
2023-07-27 07:19:23 -0300

    ✨ Open link in browser

---
+++ pkg/tui/loadlinks/keymap.go
@@ -0,0 +1,79 @@
+package loadlinks
+
+import (
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type delegateKeyMap struct {
+	open key.Binding
+}
+
+func newItemDelegate(keys *delegateKeyMap) list.DefaultDelegate {
+	d := list.NewDefaultDelegate()
+
+	d.UpdateFunc = func(msg tea.Msg, m *list.Model) tea.Cmd {
+		// var title string
+		var item Link
+
+		if i, ok := m.SelectedItem().(Link); ok {
+			item = i
+		} else {
+			return nil
+		}
+
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch {
+			case key.Matches(msg, keys.open):
+				return tea.Batch(
+					func() tea.Msg {
+						return item
+					},
+				)
+			}
+		}
+
+		return nil
+	}
+
+	help := []key.Binding{keys.open}
+
+	d.ShortHelpFunc = func() []key.Binding {
+		return help
+	}
+
+	d.FullHelpFunc = func() [][]key.Binding {
+		return [][]key.Binding{help}
+	}
+
+	return d
+}
+
+func newDelegateKeyMap() *delegateKeyMap {
+	return &delegateKeyMap{
+		open: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter", "open link"),
+		),
+	}
+}
+
+// Additional short help entries. This satisfies the help.KeyMap interface and
+// is entirely optional.
+func (d delegateKeyMap) ShortHelp() []key.Binding {
+	return []key.Binding{
+		d.open,
+	}
+}
+
+// Additional full help entries. This satisfies the help.KeyMap interface and
+// is entirely optional.
+func (d delegateKeyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{
+			d.open,
+		},
+	}
+}
