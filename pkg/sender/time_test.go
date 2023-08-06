0e8a0e9ab6ce931d3cbe69300945c8444f98526a
Vinicius Reis <luiz.moreira@willbank.com.br>
2023-08-06 01:05:27 -0300

    ✅ Add TestCalculeSendInterval

---
+++ pkg/sender/time_test.go
@@ -0,0 +1,76 @@
+package sender_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vinicius73/gamer-feed/pkg/sender"
+)
+
+func TestCalculeSendInterval(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		count int
+		want  time.Duration
+	}{
+		{
+			count: 1,
+			want:  time.Second,
+		},
+		{
+			count: 2,
+			want:  time.Second,
+		},
+		{
+			count: 5,
+			want:  time.Second,
+		},
+		{
+			count: 9,
+			want:  time.Second,
+		},
+		{
+			count: 10,
+			want:  time.Second * 2,
+		},
+		{
+			count: 11,
+			want:  time.Second * 2,
+		},
+		{
+			count: 15,
+			want:  time.Second * 2,
+		},
+		{
+			count: 18,
+			want:  time.Second * 2,
+		},
+		{
+			count: 19,
+			want:  time.Second * 2,
+		},
+		{
+			count: 20,
+			want:  time.Second * 3,
+		},
+		{
+			count: 30,
+			want:  time.Second * 3,
+		},
+		{
+			count: 60,
+			want:  time.Second * 3,
+		},
+	}
+
+	for _, tt := range tests {
+		actual := tt
+		t.Run(fmt.Sprintf("CalculeSendInterval(%v) shold be %v", actual.count, actual.want), func(t *testing.T) {
+			got := sender.CalculeSendInterval(actual.count)
+			assert.Equal(t, actual.want, got)
+		})
+	}
+}
