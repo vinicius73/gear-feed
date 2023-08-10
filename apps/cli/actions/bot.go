06e429226875aed5c0ac3b354874d2729b2a2d81
Vinicius Reis <luiz.moreira@willbank.com.br>
2023-08-10 03:31:11 -0300

    🔊 Add log

--- apps/cli/actions/bot.go
+++ apps/cli/actions/bot.go
@@ -17,6 +17,9 @@ func BotWorker(ctx context.Context) error {
 
 	db, err := database.Open(ctx, config.Storage)
 	if err != nil {
+		logger.Error().Err(err).
+			Interface("storage", config.Storage).
+			Msg("Failed to open database")
 		return err
 	}
 
